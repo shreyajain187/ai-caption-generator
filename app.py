4a5e93ab006e1bc4c545c0ead94691e7d530308b
your_name <shreyaajain187@gmail.com>
2026-03-31 10:43:20 +0530

    prepare for deployement

--- app.py
+++ app.py
@@ -116,4 +116,6 @@ def output_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
